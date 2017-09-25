d358ccec8b08ab6c2005ea5951973910d6ff0f40
7h1b0 <wyrm1992@laposte.net>
2017-09-25 17:36:37 +0200

    Remove REDUX_DEVTOOLS_EXTENSION from production

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const initialState =
+  process.env.NODE_ENV === 'production'
+    ? undefined
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__();
+
 export default () =>
-  createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk),
-  );
+  createStore(rootReducer, initialState, applyMiddleware(thunk));
